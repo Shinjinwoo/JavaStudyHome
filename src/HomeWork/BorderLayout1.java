5a9028cdbcaa9ac6fdf7fe2bb90292bca2a14102
신진우 <@DESKTOP-1G2HJN4>
2020-10-09 16:27:16 +0900

    숙제 첫번째 커밋

---
+++ src/HomeWork/BorderLayout1.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+import javax.swing.JFrame;
+
+public class BorderLayout1 extends JFrame {
+	public BorderLayout1() {
+		setTitle ("BoarderLayoutTest");
+		setSize (300,150);
+	}
+
+}
