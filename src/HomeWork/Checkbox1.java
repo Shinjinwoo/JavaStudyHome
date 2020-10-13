2dc7d0f0471528962acf88833c272f4704229444
오전반 <@1304-16>
2020-10-13 10:16:20 +0900

    2020.10.13

--- src/HomeWork/Checkbox1.java
+++ src/HomeWork/Checkbox1.java
@@ -6,7 +6,8 @@ public class Checkbox1 extends Frame  { // 프레임 상속
 	public Checkbox1() {
 		setLayout(new FlowLayout());    // 플로우 레이아웃 선언
 		Label lab = new Label("좋아하는 계절을 선택하세요"); // 레이블 선언
-		Checkbox cb1 = new Checkbox("봄");  // 체크박스 ( 말그대로 체크박스 ) 봄,여름,가을,겨울 선언
+		Checkbox cb1 = new Checkbox("봄");  // 체크박스 ( 말그대로 체크박스 ) 
+		//봄,여름,가을,겨울 선언
 		Checkbox cb2 = new Checkbox("여름");
 		Checkbox cb3 = new Checkbox("가을");
 		Checkbox cb4 = new Checkbox("겨울");
