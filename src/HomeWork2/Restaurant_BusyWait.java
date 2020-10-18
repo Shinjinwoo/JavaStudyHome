4b984d7a893c7f0e9202916704044ce7ff6928e6
Shinjinwoo <jinu712@gmail.com>
2020-10-18 17:40:59 +0900

    2020.10.18 1.클래스 중복 해결

--- src/HomeWork2/Restaurant_BusyWait.java
+++ src/HomeWork2/Restaurant_BusyWait.java
@@ -3,7 +3,7 @@
 public class Restaurant_BusyWait {
 	public static void main(String[] args) {
 		Food_BusyWait food = new Food_BusyWait();
-		Waiter_BusyWait1 Waiter =new Waiter_BusyWait1("웨이터",food);
+		Waiter_BusyWait Waiter =new Waiter_BusyWait("웨이터",food);
 		Chef_BusyWait Chef = new Chef_BusyWait("주방장",food);
 		Waiter.start();
 		Chef.start();
