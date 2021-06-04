c4eea3f798639800b966f8ab72ea1feb2553ff96
Shinjinwoo <jinu712@gmail.com>
2021-06-05 01:46:06 +0900

    2021-06-05 코딩테스트

--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -18,21 +18,41 @@ class Main {
 		
 		for(char x : s.toCharArray()) {
 			map.put(x, map.getOrDefault(x,0)+1);
+			
 			//get은 키의 벨류값을 가져옴.
 			//getOrDefault는 x의 키값이 없을 경우 0을 리턴
 			//가령 키 B에 2라는 벨류 값이 존재 할 경우 +1을 추가해서 3을 리턴하거나 키가 존재하지 않을경우 0을 리턴
 			//또는 E라는 존재하지 않는 키가 새로 들어오게 된다면 1을 리턴해서 새로운 키벨류쌍의 데이터를 해쉬테이블에 넣음.
+			
 		}
 		
 		for(char key : map.keySet()) {
 			//존재하는 키들을 전부 탐색
+			
+			
+			//map.contaionKey('F')
+			//캡에 선택한 키가 있는지 물어보는 메서드
+			
+			
+			//map.size()
+			//키의 종류별 개수를 알려줌.
+			//중복 X
+			
+			
+			//map.remove('A');
+			//A라는 특정키를 삭제함
+			
+			
+			
+			
 			
 			if(map.get(key) > max ) {
 				max=map.get(key);
 				answer=key;
 				
 			}
-			System.out.println(key + " " + map.get(key));
+			/* System.out.println(key + " " + map.get(key)); */
+			
 		}
 		return answer;
 	}
